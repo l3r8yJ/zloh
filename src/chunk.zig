b760b68b21b050db78c2a0b7a6ba5f6056a9b685
l3r8yJ <l3r8y@duck.com>
2025-04-30 06:37:19 +0300

    refactor: chunk improved

--- src/chunk.zig
+++ src/chunk.zig
@@ -1,11 +1,13 @@
 const std = @import("std");
 
-const OpCode = enum {
+pub const OpCode = enum {
     OP_RETURN,
 };
 
 pub const Chunk = struct {
-    code: *u8,
+    const Self = @This();
+
+    code: []u8,
     capacity: usize,
     count: usize,
 
@@ -13,7 +15,20 @@ pub const Chunk = struct {
         return Chunk{
             .count = 0,
             .capacity = 0,
-            .code = null,
+            .code = undefined,
         };
+    }
+
+    pub fn write(this: *Self, allocator: std.mem.Allocator, byte: u8) !void {
+        if (this.capacity < this.count + 1) {
+            this.capacity = growCapacity(this.capacity);
+            this.code = try allocator.realloc(this.code, this.capacity);
+        }
+        this.code[this.count] = byte;
+        this.count = this.count + 1;
+    }
+
+    inline fn growCapacity(capacity: usize) usize {
+        return if (capacity < 8) 8 else capacity * 2;
     }
 };
--- src/tests.zig
+++ src/tests.zig
@@ -1,10 +1,32 @@
 const std = @import("std");
 const expectEqual = std.testing.expectEqual;
 
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const Chunk = @import("chunk.zig").Chunk;
 
 test "chunk: initalizes an empty chunk" {
     const empty = Chunk.init();
-    const expected = Chunk{ .code = null, .capacity = 0, .count = 0 };
+    const expected = Chunk{ .code = undefined, .capacity = 0, .count = 0 };
     try expectEqual(expected, empty);
+}
+
+test "chunk: reallocates the memory with correct capacity" {
+    var chunk = Chunk.init();
+    const allocator = gpa.allocator();
+    try chunk.write(allocator, 0x10);
+    try expectEqual(chunk.capacity, 8);
+}
+
+test "chunk: reallocates the memory with correct count" {
+    var chunk = Chunk.init();
+    const allocator = gpa.allocator();
+    try chunk.write(allocator, 0x10);
+    try expectEqual(chunk.count, 1);
+}
+
+test "chunk: reallocates the memory with correct code" {
+    var chunk = Chunk.init();
+    const allocator = gpa.allocator();
+    try chunk.write(allocator, 0x1a);
+    try expectEqual(26, chunk.code[0]);
 }
